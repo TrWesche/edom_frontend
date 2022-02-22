9022c4cb433b81214bd1733ae3b1ade9e40a2c53
TrWesche <TrWesche@live.com>
2022-02-21 18:05:13 -0600

    Rough in Group Profile page

--- src/components/group/GroupProfile.tsx
+++ src/components/group/GroupProfile.tsx
@@ -1,19 +1,222 @@
+// React
+import React, { useEffect, MouseEvent } from 'react';
+import { NavigateFunction, useNavigate, useParams } from 'react-router-dom';
+
+// Redux
+import { useSelector, useDispatch, RootStateOrAny } from 'react-redux';
+
+// Material UI
 import {
     Grid,
-    Typography
+    Typography,
+    Card,
+    CardActionArea,
+    CardMedia,
+    CardContent,
+    Box,
+    Avatar
 } from "@mui/material"
 
 
-const GroupProfile = () => {
+// Providers
+import { authToken, useAuth } from '../../providers/authProvider';
+import { useAlert } from '../../providers/alertProvider';
+
+// Interface Imports
+import EquipCardListHorizontal, { EquipListProps } from '../building_blocks/equip/EquipCardListHorizontal';
+import {  fetchEquipListGroup } from '../../redux/actions/actEquipList';
+import { GroupObjectProps } from '../../interfaces/globalInterfaces';
+import { fetchGroupProfile } from '../../redux/actions/actGroup';
+import { fetchRoomListGroup } from '../../redux/actions/actRoomList';
+import { fetchUserListGroup } from '../../redux/actions/actUserList';
+import RoomCardListHorizontal, { RoomListProps } from '../building_blocks/room/RoomCardListHorizontal';
+
+
+interface ReduxDataPayload {
+    group: GroupObjectProps
+    // users: UserListProps
+    rooms: RoomListProps
+    equips: EquipListProps
+};
+
+
+interface ClickEvent extends MouseEvent<HTMLButtonElement> {
+    target: ClickTarget
+};
+
+interface ClickTarget extends EventTarget {
+    href?: string
+};
+
+const handleClick = (e: ClickEvent, navigate: NavigateFunction, target: string) => {
+    e.preventDefault();
+    // console.log(`Clicked: ${target}`)
+    if (target !== "") {
+        navigate(target);
+    } else {
+        console.log("Error, destination not defined")
+    }
+};
+
+
+const GroupProfileHeader = (navigate: NavigateFunction) => {
     return (
-        <Grid container spacing={2}>
-            <Grid item xs={12}>
-                <Typography>This will be the Group Profile Page!</Typography>
-                <Typography>How Exciting!</Typography>
+        <React.Fragment>
+            <Grid item container width={'100%'}>
+                <Box>
+                    <Avatar 
+                        alt="User Image"
+                        src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
+                        sx={{ width: 152, height: 152 }}
+                    />
+                </Box>
+
+                <Box>
+                    <Typography 
+                        color={'secondary.dark'} 
+                        variant='h5' 
+                        margin={"1rem 0rem 0rem 0rem"}
+                        bgcolor={'grey.A200'}
+                        padding={"0.25rem"}
+                        borderRadius={"0.25rem"}
+                    >
+                        Group Name
+                    </Typography>
+                </Box>
+
+                <Box>
+                    <Typography 
+                        color={'secondary.dark'} 
+                        variant='h5' 
+                        margin={"1rem 0rem 0rem 0rem"}
+                        bgcolor={'grey.A200'}
+                        padding={"0.25rem"}
+                        borderRadius={"0.25rem"}
+                    >
+                        Group Location
+                    </Typography>
+                </Box>
+
+                <Box>
+                    <Typography 
+                        color={'secondary.dark'} 
+                        variant='h5' 
+                        margin={"1rem 0rem 0rem 0rem"}
+                        bgcolor={'grey.A200'}
+                        padding={"0.25rem"}
+                        borderRadius={"0.25rem"}
+                    >
+                        Group Headline
+                    </Typography>
+                </Box>
+
+                <Box>
+                    <Typography 
+                        color={'secondary.dark'} 
+                        variant='h5' 
+                        margin={"1rem 0rem 0rem 0rem"}
+                        bgcolor={'grey.A200'}
+                        padding={"0.25rem"}
+                        borderRadius={"0.25rem"}
+                    >
+                        Group Description
+                    </Typography>
+                </Box>
             </Grid>
-            <Grid item xs={12}>
-                <Typography>Stuff about the individual group</Typography>
-            </Grid>
+        </React.Fragment>
+    )
+};
+
+
+
+const PageLoadHandler = (props: {
+    authData: authToken, 
+    navigate: NavigateFunction,
+    alertSetter: Function | undefined, 
+    reduxData: ReduxDataPayload}) => {
+    const { authData, navigate, alertSetter, reduxData } = props; 
+
+
+    const pageLoaded = () => {
+        return (
+            <React.Fragment>
+                {GroupProfileHeader(navigate)}
+                <Grid item container width={'100%'} margin={'2rem 0 0 0'}>
+                    <Typography variant='h4' color={'text.primary'}>Group Users</Typography>
+                </Grid>
+                {/* {UserCardListHorizontal("group-users", 4, reduxData.users)} */}
+                <Grid item container width={'100%'} margin={'2rem 0 0 0'}>
+                    <Typography variant='h4' color={'text.primary'}>Group Rooms</Typography>
+                </Grid>
+                {RoomCardListHorizontal("group-equip", 4, reduxData.rooms)}
+                <Grid item container width={'100%'} margin={'2rem 0 0 0'}>
+                    <Typography variant='h4' color={'text.primary'}>Group Equip</Typography>
+                </Grid>
+                {EquipCardListHorizontal("group-equip", 4, reduxData.equips)}
+            </React.Fragment>
+            
+        );
+    };
+
+
+    // User Logged In Check
+    if (!authData.logged_in && authData.init) {
+        if (alertSetter) {
+            alertSetter({open: true, content: "Please Login to Continue.", severity: "error"})
+        };
+        navigate('/');
+    };
+
+    return (
+        <React.Fragment>
+            {pageLoaded()}
+        </React.Fragment>
+    );
+};
+
+
+
+const GroupProfile = () => {
+    // React / Redux Function Instantiations
+    const navigate = useNavigate();
+
+    // Context Providers
+    const { alertSetter } = useAlert();
+    const { authData } = useAuth();
+
+    const params: any = useParams();
+
+    // React / Redux Function Instantiations
+    const dispatch = useDispatch();
+
+    const reduxGroup: GroupObjectProps = useSelector((store: RootStateOrAny) => store?.redGroup);
+    // const reduxUserList: UserListProps = useSelector((store: RootStateOrAny) => store?.redUserList);
+    const reduxRoomList: RoomListProps = useSelector((store: RootStateOrAny) => store?.redRoomList);
+    const reduxEquipList: EquipListProps = useSelector((store: RootStateOrAny) => store?.redEquipList);
+
+
+    useEffect(() => {
+        dispatch(fetchGroupProfile(params.groupID, authData));
+        // dispatch(fetchUserListGroup(params.groupID));
+        dispatch(fetchRoomListGroup(params.groupID));
+        dispatch(fetchEquipListGroup(params.groupID));
+    }, [dispatch]);
+
+    const reduxData: ReduxDataPayload = {
+        group: reduxGroup,
+        // users: reduxUserList,
+        rooms: reduxRoomList,
+        equips: reduxEquipList
+    };
+
+    return (
+        <Grid container spacing={2} justifyContent={'center'} width={'100%'}>
+            <PageLoadHandler 
+                authData={authData}
+                navigate={navigate}
+                alertSetter={alertSetter}
+                reduxData={reduxData}
+            />
         </Grid>
     )
 };
---
+++ src/redux/actions/actGroup.ts
@@ -0,0 +1,56 @@
+// https://medium.com/@killerchip0/handling-asynchronous-fetching-of-data-with-react-redux-2aecc65e87af
+
+import { Dispatch } from "redux";
+import { GroupObjectProps } from "../../interfaces/globalInterfaces";
+import { authToken} from "../../providers/authProvider";
+
+import apiEDOM from "../../utils/apiEDOM";
+import {
+    GROUP_ACTIONS,
+    ERROR
+} from "../actionDictionary";
+
+
+
+const fetchGroupProfile = (groupID: string, authData: authToken | undefined) => {
+    return async function (dispatch: Dispatch) {
+        dispatch(startFetchGroupProfile());
+        try {
+            if (!authData?.username) {
+                throw new Error ("Current User Authentication Data Malformed | User Not Logged In");
+            };
+
+            const result = await apiEDOM.getGroup(groupID);
+            const data = result.data;
+            dispatch(gotGroupProfile(data));
+            
+        } catch (error) {
+            console.log("Redux Error Caught");
+            dispatch(gotError());
+        }
+    }
+};
+
+const startFetchGroupProfile = () => {
+    return ({
+        type: GROUP_ACTIONS.START_GROUP_LOAD
+    });
+};
+
+const gotGroupProfile = (groupData: GroupObjectProps) => {
+    return ({
+        type: GROUP_ACTIONS.FINISH_GROUP_LOAD,
+        payload: groupData
+    })
+};
+
+
+const gotError = () => {
+    return ({
+        type: ERROR
+    })
+};
+
+export {
+    fetchGroupProfile
+}
