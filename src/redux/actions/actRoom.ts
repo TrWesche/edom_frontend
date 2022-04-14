b7d71eee46bc181ff48358df5d46fa4494c37ded
TrWesche <TrWesche@live.com>
2022-04-14 18:04:24 -0400

    Room Action Rough Updates

--- src/redux/actions/actRoom.ts
+++ src/redux/actions/actRoom.ts
@@ -1,25 +1,27 @@
 // https://medium.com/@killerchip0/handling-asynchronous-fetching-of-data-with-react-redux-2aecc65e87af
 
+// Libraries
 import { Dispatch } from "redux";
-import { RoomObjectProps } from "../../interfaces/globalInterfaces";
-import { authToken} from "../../providers/authProvider";
 
+// Interfaces
+import { RequestRoomObject } from "../../interfaces/edomRoomInterfaces";
+
+// API
 import apiEDOM from "../../utils/apiEDOM";
+
+// Utilities
+
+// Redux Actions
 import {
     ROOM_ACTIONS,
     ERROR
 } from "../actionDictionary";
 
 
-
-const fetchRoomProfile = (roomID: string, authData: authToken | undefined) => {
+const fetchRoomProfile = (roomID: string) => {
     return async function (dispatch: Dispatch) {
         dispatch(startFetchRoomProfile());
         try {
-            if (!authData?.username) {
-                throw new Error ("Current User Authentication Data Malformed | User Not Logged In");
-            };
-
             const result = await apiEDOM.getRoom(roomID);
             const data = result.data.room;
             dispatch(gotRoomProfile(data));
@@ -37,7 +39,7 @@ const startFetchRoomProfile = () => {
     });
 };
 
-const gotRoomProfile = (roomData: RoomObjectProps) => {
+const gotRoomProfile = (roomData: RequestRoomObject) => {
     return ({
         type: ROOM_ACTIONS.FINISH_ROOM_LOAD,
         payload: roomData
