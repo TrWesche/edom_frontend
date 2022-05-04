e2b3c02aa494bb3bf76baa77e3ab7193ed15284a
TrWesche <TrWesche@live.com>
2022-05-04 19:11:28 -0400

    Update Authenticated home with new Card List

--- src/components/tier02/landing/AuthenticatedHome.tsx
+++ src/components/tier02/landing/AuthenticatedHome.tsx
@@ -27,14 +27,30 @@ import HandleButtonClick from '../../../utils/HandleButtonClick';
 import { fetchGroupList } from '../../../redux/actions/actGroupList';
 import { fetchRoomList } from '../../../redux/actions/actRoomList';
 
+// Component Imports
+import CardList, { CardListProps } from '../cardlist/CardList';
+import { GroupListProps } from "../cardlist/GroupCardListHorizontal";
+import { RoomListProps } from "../cardlist/RoomCardListHorizontal";
+import { EquipListProps } from '../cardlist/EquipCardListHorizontal';
 
-import GroupCardListHorizontal, { GroupListProps } from "../cardlist/GroupCardListHorizontal";
-import RoomCardListHorizontal, { RoomListProps } from "../cardlist/RoomCardListHorizontal";
 
+const CardRenderProps = {
+    xlRows: 1,
+    lgRows: 1,
+    mdRows: 2,
+    smRows: 3,
+    xsRows: 6,
+    xlColumns: 4,
+    lgColumns: 4,
+    mdColumns: 3,
+    smColumns: 2,
+    xsColumns: 1
+};
 
 interface ReduxDataPayload {
-    groups: GroupListProps
-    rooms: RoomListProps
+    group: CardListProps
+    room: CardListProps
+    equip: CardListProps
 };
 
 
@@ -90,9 +106,9 @@ const PageLoadHandler = (props: {
         authData: authToken,
         navigate: NavigateFunction,
         alertSetter: Function | undefined, 
-        reduxData: ReduxDataPayload}) => {
-    const { authData, navigate, alertSetter, reduxData } = props; 
-
+        renderData: ReduxDataPayload}) => {
+    const { authData, navigate, alertSetter, renderData } = props; 
+    const {group, room, equip} = renderData;
 
     const pageLoaded = () => {
         return (
@@ -101,11 +117,39 @@ const PageLoadHandler = (props: {
                 <Grid item xs={12} margin={"20px 0 0 0"}>
                     <Typography variant='h2' color={'text.primary'}>Featured Groups</Typography>
                 </Grid>
-                {GroupCardListHorizontal(navigate, "featured-groups", 3, reduxData.groups)}
+                <CardList 
+                    listid={group.listid}
+                    cardType={group.cardType}
+                    navigate={group.navigate}
+                    cardContent={group.cardContent}
+                    renderConfig={group.renderConfig}
+                    displayIsProcessing={group.displayIsProcessing}
+                    displayError={group.displayError}
+                />
                 <Grid item xs={12} margin={"20px 0 0 0"}>
                     <Typography variant='h2' color={'text.primary'}>Featured Rooms</Typography>
                 </Grid>
-                {RoomCardListHorizontal(navigate, "featured-rooms", 3, reduxData.rooms)}
+                <CardList 
+                    listid={room.listid}
+                    cardType={room.cardType}
+                    navigate={room.navigate}
+                    cardContent={room.cardContent}
+                    renderConfig={room.renderConfig}
+                    displayIsProcessing={room.displayIsProcessing}
+                    displayError={room.displayError}
+                />
+                <Grid item xs={12} margin={"20px 0 0 0"}>
+                    <Typography variant='h2' color={'text.primary'}>Featured Equipment</Typography>
+                </Grid>
+                <CardList 
+                    listid={equip.listid}
+                    cardType={equip.cardType}
+                    navigate={equip.navigate}
+                    cardContent={equip.cardContent}
+                    renderConfig={equip.renderConfig}
+                    displayIsProcessing={equip.displayIsProcessing}
+                    displayError={equip.displayError}
+                />
             </React.Fragment>
             
         );
@@ -135,15 +179,51 @@ const AuthenticatedHome = (props: {authData: authToken, navigate: NavigateFuncti
     
     const reduxGroupList: GroupListProps = useSelector((store: RootStateOrAny) => store?.redGroupList);
     const reduxRoomList: RoomListProps = useSelector((store: RootStateOrAny) => store?.redRoomList);
+    const reduxEquipList: EquipListProps = useSelector((store: RootStateOrAny) => store?.redEquipList);
+
+    const groupCardContentList = buildGroupContentList(reduxGroupList);
+    const roomCardContentList = buildRoomContentList(reduxRoomList);
+    const equipCardContentList = buildEquipContentList(reduxEquipList);
+
+    const groupCardListData: CardListProps = {
+        listid: `featured-group-list`,
+        cardType: "horizontal",
+        navigate: navigate,
+        cardContent: groupCardContentList,
+        renderConfig: CardRenderProps,
+        displayIsProcessing: reduxGroupList.isProcessing,
+        displayError: reduxGroupList.error
+    };
+
+    const roomCardListData: CardListProps = {
+        listid: `featured-room-list`,
+        cardType: "horizontal",
+        navigate: navigate,
+        cardContent: roomCardContentList,
+        renderConfig: CardRenderProps,
+        displayIsProcessing: reduxRoomList.isProcessing,
+        displayError: reduxRoomList.error
+    };
+
+    const equipCardListData: CardListProps = {
+        listid: `featured-equip-list`,
+        cardType: "horizontal",
+        navigate: navigate,
+        cardContent: equipCardContentList,
+        renderConfig: CardRenderProps,
+        displayIsProcessing: reduxEquipList.isProcessing,
+        displayError: reduxEquipList.error
+    };
 
     useEffect(() => {
         dispatch(fetchGroupList());
         dispatch(fetchRoomList());
     }, [dispatch]);
 
-    const reduxData: ReduxDataPayload = {
-        groups: reduxGroupList,
-        rooms: reduxRoomList
+    const renderData: ReduxDataPayload = {
+        group: groupCardListData,
+        room: roomCardListData,
+        equip: equipCardListData
     };
 
     return (
@@ -151,9 +231,122 @@ const AuthenticatedHome = (props: {authData: authToken, navigate: NavigateFuncti
             authData={authData}
             navigate={navigate}
             alertSetter={alertSetter}
-            reduxData={reduxData}
+            renderData={renderData}
         />
     )
 }
 
 export default AuthenticatedHome
+
+
+
+
+const buildEquipContentList = (data: EquipListProps ) => {
+    const retList: any = [];
+    if (!data.equip) {
+        return retList;
+    }
+    data.equip.forEach(element => {
+        retList.push({
+            settings: {
+                displayEdit: true,
+                displayMedia: true,
+                mediaHeight: 200,
+                // mediaWidth: 200,
+                displayContent: true,
+                contentHeight:  100,
+                displayActions: false,
+                actionHeight: 100,
+                enableActionArea: true
+            },
+            data: {
+                editAllowed: element.edit_permissions || false,
+                editButtonDestination: `/equip/${element.id}` || `#`,
+                actionAreaDestination: `/equip/${element.id}` || `#`,
+                mediaURI: element.image_url || `Image Not Found`,
+                mediaAltText: "TODO - Alt Text Not Stored",
+                contentTexts: [
+                    {textVariant: "h5", textContent: element.name}, 
+                    {textVariant: "body2", textContent: element.headline}, 
+                    // {textVariant: "body2", textContent: element.description}
+                ]
+            }
+        })
+    });
+
+    return retList;
+};
+
+
+const buildRoomContentList = (data: RoomListProps ) => {
+    const retList: any = [];
+    if (!data.rooms) {
+        return retList;
+    }
+    data.rooms.forEach(element => {
+        retList.push({
+            settings: {
+                displayEdit: true,
+                displayMedia: true,
+                mediaHeight: 200,
+                // mediaWidth: 200,
+                displayContent: true,
+                contentHeight:  100,
+                displayActions: false,
+                actionHeight: 100,
+                enableActionArea: true
+            },
+            data: {
+                editAllowed: element.edit_permissions || false,
+                editButtonDestination: `/equip/${element.id}` || `#`,
+                actionAreaDestination: `/equip/${element.id}` || `#`,
+                mediaURI: element.image_url || `Image Not Found`,
+                mediaAltText: "TODO - Alt Text Not Stored",
+                contentTexts: [
+                    {textVariant: "h5", textContent: element.name}, 
+                    {textVariant: "body2", textContent: element.headline}, 
+                    // {textVariant: "body2", textContent: element.description}
+                ]
+            }
+        })
+    });
+
+    return retList;
+};
+
+
+const buildGroupContentList = (data: GroupListProps ) => {
+    const retList: any = [];
+    if (!data.group) {
+        return retList;
+    }
+    data.group.forEach(element => {
+        retList.push({
+            settings: {
+                displayEdit: true,
+                displayMedia: true,
+                mediaHeight: 200,
+                // mediaWidth: 200,
+                displayContent: true,
+                contentHeight:  100,
+                displayActions: false,
+                actionHeight: 100,
+                enableActionArea: true
+            },
+            data: {
+                editAllowed: element.edit_permissions || false,
+                editButtonDestination: `/groups/${element.id}` || `#`,
+                actionAreaDestination: `/groups/${element.id}` || `#`,
+                mediaURI: element.image_url || `Image Not Found`,
+                mediaAltText: "TODO - Alt Text Not Stored",
+                contentTexts: [
+                    {textVariant: "h5", textContent: element.name}, 
+                    {textVariant: "body2", textContent: element.headline}, 
+                    // {textVariant: "body2", textContent: element.description}
+                ]
+            }
+        })
+    });
+
+    return retList;
+};
